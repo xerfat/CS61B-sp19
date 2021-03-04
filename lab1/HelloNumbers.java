823a235700984c8ae4e6c95be43c171d888f47f2
crabwj <crabwj@Crab-MacBook.local>
2021-03-04 21:41:14 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,27 @@
+package Lecture01;
+
+public class HelloNumbers {
+    public static void main(String[] args){
+        int x;
+        x = 0;
+        // or int x = 0;
+        int sum = 0;
+        while (x < 10){
+            // System.out.println(x + " ");
+            // System.out.print(x + " ");
+            // Our print statement is just System.out.print instead of System.out.println.
+            // This means we should not include a newline (a return).
+            sum = sum + x;
+            System.out.print(sum + " ");
+            x = x + 1;
+        }
+        // x = "horse";  //java: 不兼容的类型: java.lang.String无法转换为int
+    }
+}
+    // In Java, variables have specific type, which can never change. This is called static typing
+    /*
+    1. Before Java variables can be used, they must be declared.
+    2. Java variables must have a specific type.
+    3. Java variables can never change.
+    4. Types are verified before the code even runs !!! (different from Python)
+     */
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,31 @@
+package Lecture01;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+
+public class HelloWorld {
+    public static void main(String[] args){
+    // public 这是一个公有的方法
+    // static 这是一个静态的方法
+    // void 没有返回值
+    // main 方法名
+    // String[] args，这是参数，程序运行的时候可以加上参数的，参数的类型是字符串数组，放在args这个参数中
+        System.out.println("Hello World!");
+    }
+}
+    // Java considers classes very important !!!!
+    // All code must be in classes, which forces you to be object-oriented
+
+
+    // Java vs Python
+    // In general, Java is much faster than Python, though the startup time for a new Java program is longer
+
+    /*
+    Teaching conclusion:
+    1. In Java, all code must be part of a class.
+    2. Classes are defined with "public class CLASSNAME"
+    3. We use '{ }'(curly braces) to delineate the beginning and ending of things.
+    4. We must end lines with a ';'(semicolon)
+    5. The code we want to run must be inside 'public static void main(String[] args)'
+     */
